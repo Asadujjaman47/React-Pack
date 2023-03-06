c93d56da7b9c5e8e7a30fe72bcce7bbccd7a598b
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-03-06 15:39:51 +0600

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
 
 function App() {
+  const name = "Asad";
+  const element = <p> I am a JSX</p>;
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Hello {name}</h1>
+      <p className="para">{name == "Asad" ? "true" : "false"}</p>
+      <input type="text" placeholder="" name="" />
+      {element}
     </div>
   );
 }
