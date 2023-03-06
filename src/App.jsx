28ddb7df59b9f37f136cb8eb983c055a82f2104c
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-03-06 16:30:32 +0600

    Forms example

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  function clicked(name) {
-    alert("I am clicked! " + name);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  function handleSubmit() {
+    alert("Your name is " + name + "and you are " + age + " years old");
   }
 
   return (
-    <div className="container">
-      <button onClick={clicked}>Click Me</button>
-
-      {/* for pass argument, use arrow function  */}
-      <button onClick={() => clicked("Asad")}>Click Me</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">
+        Enter Your name:
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          name="name"
+          value={name}
+        />
+      </label>
+      <br />
+      <label htmlFor="age">
+        Enter Your age:
+        <input
+          onChange={(e) => setAge(e.target.value)}
+          type="text"
+          name="age"
+          id="age"
+          value={age}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
