09859f22a67189c7b8ea1056473052fae306b19b
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-03-06 19:51:07 +0600

    Routing in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { useRef } from "react";
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import About from "../src/components/About";
+import Contact from "../src/components/Contact";
+import ErrorPage from "../src/components/ErrorPage";
+import Home from "../src/components/Home";
+import Services from "../src/components/Services";
+import Navbar from "./components/Navbar";
+
 function App() {
-  const inputRef = useRef();
-
-  function focusInput() {
-    inputRef.current.focus();
-  }
-
   return (
-    <>
-      <input type="text" ref={inputRef} />
-      <button onClick={focusInput}>Focus Input</button>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <h1>Routing in React</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/services" element={<Services />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
