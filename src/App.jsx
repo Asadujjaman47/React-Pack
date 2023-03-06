d8b3770e72d9e7974c2a155201ca5d4d11815bbf
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-03-06 21:42:17 +0600

    Digital clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+
+  const [currentTime, setCurrentTime] = useState(time);
+
+  function updateTime() {
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  }
+
+  setInterval(updateTime, 1000);
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>{time}</h1>
     </div>
   );
 }
