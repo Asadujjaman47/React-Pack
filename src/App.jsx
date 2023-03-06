7ec2bffa33922ea9099fcc08aaa607478a4c024f
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-03-06 16:13:06 +0600

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./App.css";
-import Parent from "./components/Parent";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function CountUpdate() {
+    setCount(count + 1);
+  }
+
   return (
-    <div>
-      <Parent />
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={CountUpdate}>Count</button>
     </div>
   );
 }
